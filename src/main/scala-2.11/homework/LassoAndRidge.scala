be165b931e7f8c92736bc9e1aa5d62c627071190
mrjus <sardor.abdullaev@trademachines.com>
2017-05-01 23:07:46 +0200

    Completed Homework 1, 2.1

--- src/main/scala-2.11/homework/LassoAndRidge.scala
+++ src/main/scala-2.11/homework/LassoAndRidge.scala
@@ -1,9 +1,101 @@
 package homework
 
+import org.apache.spark.ml.feature.{OneHotEncoder, StandardScaler, StringIndexer, VectorAssembler}
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.mllib.regression.RidgeRegressionWithSGD
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.util.Random
 /**
   * Created by Sardor on 4/26/2017.
   */
 object LassoAndRidge {
 
+  val sparkSession: SparkSession = SparkSession.builder.
+    master("local")
+    .appName("spark session example")
+    .getOrCreate()
+  import sparkSession.implicits._
+//read file and remove NAs
+  val df: DataFrame = sparkSession
+    .read.option("header", "true").option("inferSchema", "true")
+    .csv("C:\\Users\\mrjus\\IdeaProjects\\Analytics_lab_homework\\src\\main\\resources\\homework\\Hitters.csv")
+    .na.drop()
+    .filter($"Salary"!=="NA")
+
+  val allColumns = df.columns.toList
+  val categoryColumns = List("League","Division","NewLeague")
+  val salaryColumn = "Salary"
+
+  val assembler = new VectorAssembler()
+    .setInputCols(allColumns.diff(categoryColumns).diff(Seq(salaryColumn)).toArray)
+    .setOutputCol("features_for_scaling")
+
+  val featuresForScalingDf = assembler.transform(df)
+
+  val scaler = new StandardScaler()
+    .setInputCol("features_for_scaling")
+    .setOutputCol("features_scaled")
+
+  val scalerModel = scaler.fit(featuresForScalingDf)
+
+  val scaledModel = scalerModel.transform(featuresForScalingDf)
+
+  val categorisedDf = categoryColumns.foldLeft(scaledModel) {
+    (rDf,col) =>
+      val indexer = new StringIndexer()
+        .setInputCol(col)
+        .setOutputCol(col+"_index")
+        .fit(rDf)
+
+      val indexed = indexer.transform(rDf)
+
+      val encoder = new OneHotEncoder()
+        .setInputCol(col+"_index")
+        .setOutputCol(col+"_vec")
+
+      encoder.transform(indexed)
+  }
+  val assembler2 = new VectorAssembler()
+    .setInputCols(("features_scaled"::categoryColumns.map(_+"_vec")).toArray)
+    .setOutputCol("features")
+
+  val readyDataFrame= assembler2.transform(categorisedDf)
+
+  val train = readyDataFrame.select($"features",$"Salary".cast(DoubleType)).sample(withReplacement =  false,0.5,1)
+  val test = readyDataFrame.select($"features",$"Salary".cast(DoubleType)).except(train)
+
+  val lr = new LinearRegression()
+    .setLabelCol("Salary")
+    .setFeaturesCol("features")
+    .setPredictionCol("preds")
+
+  val lrModel = lr.fit(train)
+
+  println("Linear MSE = "+lrModel.summary.rootMeanSquaredError)
+
+  val lassoR = new LinearRegression()
+    .setMaxIter(10)
+    .setRegParam(0.3)
+    .setElasticNetParam(1.0)
+    .setLabelCol("Salary")
+    .setFeaturesCol("features")
+    .setPredictionCol("preds")
+
+  val lassoModel = lassoR.fit(train)
+
+  println("Lasso MSE = "+lassoModel.summary.rootMeanSquaredError)
+  val rr = new LinearRegression()
+    .setMaxIter(10)
+    .setRegParam(0.3)
+    .setElasticNetParam(0.0)
+    .setLabelCol("Salary")
+    .setFeaturesCol("features")
+    .setPredictionCol("preds")
+
+  val rrModel = rr.fit(train)
+
+  println("Ridge MSE = "+rrModel.summary.rootMeanSquaredError)
 
 }
--- src/main/scala-2.11/homework/PCAIntro.scala
+++ src/main/scala-2.11/homework/PCAIntro.scala
@@ -1,8 +1,12 @@
 package homework
 
-import org.apache.spark.ml.feature.{PCA, VectorAssembler}
+import org.apache.spark.ml.feature.{PCA, StandardScaler, VectorAssembler}
+import org.apache.spark.ml.linalg.DenseVector
 import org.apache.spark.ml.regression.LinearRegression
-import org.apache.spark.sql.functions.{variance,sum}
+import org.apache.spark.sql.{Column, Row}
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.{sum, variance}
 
 
 /**
@@ -24,7 +28,7 @@ object PCAIntro {
 
   val df: DataFrame = sparkSession
     .read.option("header", "true").option("inferSchema", "true")
-    .csv("C:\\Users\\mrjus\\IdeaProjects\\Titanic\\src\\main\\resources\\homework\\hprice1.csv")
+    .csv("C:\\Users\\mrjus\\IdeaProjects\\Analytics_lab_homework\\src\\main\\resources\\homework\\hprice1.csv")
     .na.drop()
 
   val colsNotPrice = df.columns.filter(_ != "price")
@@ -34,33 +38,67 @@ object PCAIntro {
 
   val featureDf = assembler.transform(df).select("price","features")
 
+  val scaler = new StandardScaler()
+    .setInputCol("features")
+    .setOutputCol("features_scaled")
+
+  val scalerModel = scaler.fit(featureDf)
+
+  val scaledDf = scalerModel.transform(featureDf)
   /*
   1. Fit a linear model using all variables (y = price)
   */
   val lr = new LinearRegression()
-    .setMaxIter(10)
-    .setRegParam(0.3)
-    .setElasticNetParam(0.8)
     .setLabelCol("price")
-    .setFeaturesCol("features")
+    .setFeaturesCol("features_scaled")
     .setPredictionCol("preds")
 
-  val lrModel = lr.fit(featureDf)
+  val lrModel = lr.fit(scaledDf)
   println("Linear MSE = "+lrModel.summary.rootMeanSquaredError)
 
   //2. Fit a linear model using just the first k PCs that “explain” >95% of the variance
 
-
   val pca = new PCA()
-    .setInputCol("features")
+    .setInputCol("features_scaled")
     .setOutputCol("pcaFeatures")
     .setK(8)
-    .fit(featureDf)
+    .fit(scaledDf)
 
-  val pcaDF = pca.transform(featureDf)
-  val pcaVar = pcaDF.select(variance("pcaFeatures"))
-  val pcaVarSum = pcaDF.select(sum(variance("pcaFeatures")))
-  //TODO do scaling
-  //for (i <- Range(0,8))
+  val pcaDF = pca.transform(scaledDf)
+
+  def vectorHead(n:Int): UserDefinedFunction = udf{ x:DenseVector => x(n) }
+  val readyDf =
+    (0 until 8).foldLeft(pcaDF){
+      (r,i)=>
+        r.withColumn("PC"+(i+1).toString, vectorHead(i)(pcaDF("pcaFeatures")))
+  }
+
+  val total_var = (0 until 8).foldLeft(0.0)((r,i)=>
+    r+readyDf.select(variance("PC"+(i+1).toString)).persist().head().getDouble(0)
+  )
+  (0 until 8).foreach {
+    i =>
+      val cumval = (0 until i).foldLeft(0.0)((r, i2) => r + readyDf.select(variance("PC" + (i2 + 1).toString)).head().getDouble(0))
+      println(cumval / total_var)
+      println("#########################")
+  }
+  val assembler2 = new VectorAssembler()
+    .setInputCols((1 until 6).map(i=>"PC"+i).toArray)
+    .setOutputCol("five_pca")
+
+  val finalDF = assembler2.transform(readyDf)
+
+  val PCAr = new LinearRegression()
+    .setLabelCol("price")
+    .setFeaturesCol("five_pca")
+    .setPredictionCol("preds")
+
+  val PCAlrModel = PCAr.fit(finalDF)
+  println("PCE MSE = "+PCAlrModel.summary.rootMeanSquaredError)
+
   //3. Compare test error of the models
+  println("Diff MSE = "+(PCAlrModel.summary.rootMeanSquaredError - lrModel.summary.rootMeanSquaredError))
+  //Linear MSE = 12.164661372084828
+  //PCA MSE = 31.07825959295135
+  //Diff MSE = 18.913598220866525
 }
